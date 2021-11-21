d78b627057a9e11801412a9241514b0d39710d98
Zky-Na <94729346+Zky-Na@users.noreply.github.com>
2021-11-22 01:43:46 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -22,7 +22,17 @@ bot.onText(/\/Date/, (msg) => {
     global_msg_id = msg.chat.id;
     bot.sendMessage(
         global_msg_id,
-        `Now ${msg.date.toUTCString()};`
+        function Unix_timestamp(t)
+{
+var dt = new Date(t*1000);
+var hr = dt.getHours();
+var m = "0" + dt.getMinutes();
+var s = "0" + dt.getSeconds();
+return hr+ ':' + m.substr(-2) + ':' + s.substr(-2);  
+}
+
+console.log(Unix_timestamp(${msg.date}));
+
     );
 });
 
