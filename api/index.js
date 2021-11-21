14d2601cc8bf5da3a6539bc847f3c3ad46f526cb
Zky-Na <94729346+Zky-Na@users.noreply.github.com>
2021-11-22 02:36:22 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -13,7 +13,7 @@ let global_msg_id;
 bot.onText(/\/Start/, (msg) => {
     bot.sendMessage(msg.chat.id, `Welcome, ${msg.chat.first_name}`, {
     "reply_markup": {
-      "keyboard":[["/Assalamualaikum"],["/Cuaca"]]
+      "keyboard":[["/Assalamualaikum"],["/Bagaimana_Kabar_Cuaca"]]
       }
     });
 });
@@ -26,7 +26,7 @@ bot.onText(/\/Assalamualaikum/, (msg) => {
         );
 });
 
-bot.onText(/\/Cuaca/, (msg) => {
+bot.onText(/\/Bagaimana_Kabar_Cuaca/, (msg) => {
     global_msg_id = msg.chat.id;
     bot.sendMessage(
         global_msg_id,
