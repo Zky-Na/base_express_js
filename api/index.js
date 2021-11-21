7db303c35d1f3c1748b9585ff4a6c3e18bd747c3
Zky-Na <94729346+Zky-Na@users.noreply.github.com>
2021-11-22 02:25:04 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -19,7 +19,7 @@ bot.onText(/\/Start/, (msg) => {
 });
 
 bot.onText(/\/Assalamualaikum/, (msg) => {
-    bot.sendMessage(msg.chat.id, `link.open(https://esp-pbm12-41421110113.herokuapp.com/api/test/Waalaikumsalam)`)
+    bot.sendMessage(msg.chat.id, ${window.open(`https://esp-pbm12-41421110113.herokuapp.com/api/test/Waalaikumsalam`)})
 });
 
 bot.on('message', (msg) => {
