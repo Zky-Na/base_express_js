cdaf87d4165028b8e138598f8c85623a2b6b4031
Zky-Na <94729346+Zky-Na@users.noreply.github.com>
2021-11-22 00:36:54 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -23,7 +23,7 @@ bot.onText(/\/Start/, (msg) => {
     bot.sendMessage(
         global_msg_id,
         `hello ${msg.chat.first_name}, welcome...\n`
-        replyMarkup: inlineKeyboard,
+        replyMarkup: inlineKeyboard
     );
 });
 
